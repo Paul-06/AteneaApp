5308e2edef1f5e0513465add8b1938c5e8e11907
mariluzramos <mariluz20033@gmail.com>
2024-03-23 17:26:52 -0500

    Subida de las vistas home and profile

--- AteneaApp/AteneaApp/Views/HomePage.xaml.cs
+++ AteneaApp/AteneaApp/Views/HomePage.xaml.cs
@@ -3,18 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace AteneaApp.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class HomePage : ContentPage
-	{
-		public HomePage ()
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        // Declara la propiedad ImageSources
+        public List<string> ImageSources { get; set; }
+        public HomePage ()
+
 		{
+
 			InitializeComponent ();
-		}
-	}
-}
+
+            ImageSources = new List<string>
+            {
+                "descuent.jpg",
+                "descuento.png",
+                "descu.png" 
+            };
+            BindingContext = this;
+        }
+
+
+        private async void Button_ClickedAsync(object sender, EventArgs e)
+		{
+            string phoneNumber = "51918993755";
+            string message = "Hola, quisiera informacion...";
+            string  url = $"whatsapp://send?phone={phoneNumber}&text={message}";
+            await Browser.OpenAsync(url);
+
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            string number = "51918993755";
+            string message = "Hola.%20Quiero%20información.";
+            string url = $"https://wa.me/{number}/?text={message}";
+
+            await Browser.OpenAsync(url);
+        }
+
+
+    }
+    }
---
+++ AteneaApp/AteneaApp/Views/ProfilePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+using System;
+using Xamarin.Essentials;
+using Xamarin.Forms.Xaml;
+
+namespace AteneaApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+      
+
+        public ProfilePage()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            string number = "51918993755";
+            string message = "Hola.%20Quiero%20información.";
+            string url = $"https://wa.me/{number}/?text={message}";
+
+            await Browser.OpenAsync(url);
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Maps());
+
+        }
+    }
+}
